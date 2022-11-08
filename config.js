862b0e8770a39605f1d1c45887dd7214fa18fc62
elsaaeid <saidsadaoy@gmail.com>
2022-11-08 17:13:10 +0200

    deploy changes

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export const PORT = process.env.PORT || 8800
 
-export const DB_HOST = process.env.DB_HOST || 'localhost'
+export const DB_HOST = process.env.DB_HOST || 'containers-us-west-73.railway.app'
 export const DB_USER = process.env.DB_USER || 'root'
-export const DB_PASSWORD = process.env.DB_PASSWORD || "root"
-export const DB_NAME = process.env.DB_NAME || 'digits'
-export const DB_PORT = process.env.DB_PORT || 3306
+export const DB_PASSWORD = process.env.DB_PASSWORD || "zcuj5feDWCZBuMf1vPH5"
+export const DB_NAME = process.env.DB_NAME || 'railway'
+export const DB_PORT = process.env.DB_PORT || 8061
